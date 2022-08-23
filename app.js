4fe5da64c8abeca26836858665c8ec77f670a1d9
rabbi <rabbisyed98@gmail.com>
2022-08-23 23:16:41 +0600

    funtionality completed

--- app.js
+++ app.js
@@ -1,1 +1,79 @@
-console.log('connect');
+    const playerArray = [];
+
+    function buttonSelector(buttons) {
+    const playerName = buttons.parentNode.children[0].innerText;
+
+    const playerObj = {
+        playerName: playerName,
+    };
+
+    playerArray.push(playerObj);
+
+    if (playerArray.length <= 5) {
+        buttons.style.background = "lightgray";
+
+        buttons.style.color = "black";
+
+        buttons.disabled = true;
+
+        const plyerList = document.getElementById("player-list");
+
+        const createList = document.createElement("li");
+
+        createList.innerText = playerName;
+
+        plyerList.appendChild(createList);
+
+        buttons.setA;
+    } else {
+        alert("Select your best five player");
+
+        playerArray.pop();
+    }
+
+    console.log(playerArray);
+    }
+
+    document.getElementById("calculate").addEventListener("click", function () {
+    const perPlayersElement = document.getElementById("per-player");
+
+    const perPlayerString = perPlayersElement.value;
+
+    const parPlayer = parseFloat(perPlayerString);
+
+    if (parPlayer >= 0) {
+        const firstCalculate = playerArray.length * parPlayer;
+
+        document.getElementById("per-player-display").innerText = firstCalculate;
+    } else {
+        alert("Please Enter a valid number!");
+    }
+
+    perPlayersElement.value = "";
+    });
+
+    document
+    .getElementById("calculate-total")
+    .addEventListener("click", function () {
+        const perPlayerOldTotal = document.getElementById("per-player-display");
+
+        const perPlayerOldTotalString = perPlayerOldTotal.innerText;
+
+        const perPlayerOld = parseFloat(perPlayerOldTotalString);
+
+        const managerElement = document.getElementById("manager-field").value;
+
+        const manager = parseFloat(managerElement);
+
+        const coachElement = document.getElementById("coach-field").value;
+
+        const coach = parseFloat(coachElement);
+
+        if (perPlayerOld >= 0 && manager >= 0 && coach >= 0) {
+        const calculateTotal = perPlayerOld + manager + coach;
+
+        document.getElementById("total-price").innerText = calculateTotal;
+        } else {
+        alert("Please Enter a valid number!");
+        }
+    });
